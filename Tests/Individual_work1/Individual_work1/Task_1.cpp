615af9092510c5bbe10cd3fbbc332a988fa0513a
kretskyy <deonisius03@gmail.com>
2021-02-27 23:58:30 +0200

    Individual_work1

---
+++ Tests/Individual_work1/Individual_work1/Task_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int a;
+	cin >> a;
+
+	cout << pow(a, 2) << endl;
+}
---
+++ Tests/Individual_work1/Individual_work1/Task_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void main1()
+{
+	int a;
+	cin >> a;
+
+	cout << pow(a, 2) << endl;
+}
+// Input: 2
+// Output: 4
