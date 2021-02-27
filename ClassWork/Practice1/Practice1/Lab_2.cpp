443fdd183f6fdb23d72044a1f3652926099ad875
kretskyy <deonisius03@gmail.com>
2021-02-27 23:57:06 +0200

    Lab_2

--- ClassWork/Practice1/Practice1/Lab_2.cpp
+++ ClassWork/Practice1/Practice1/Lab_2.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main
-{
+//type name(type1 param1, type2 param2, ...)
+//{
+//	statements1;
+//	statements2;
+//	statements3;
+//	...
+//		return val; //val of type
+//}
 
+
+//int add(int a, int b = 2)
+//{
+//	int r;
+//	r = a + b;
+//	return r;
+//}
+//
+//void print(const int& a)
+//{
+//	cout << a << endl;
+//}
+//
+//void print(int* a, int len)
+//{
+//
+//}
+//
+//void duplicate(int& a)
+//{
+//	a *= 2;
+//}
+
+int main()
+{
+	//type res = name(a, b, c);
+
+	//int x = 5, y = 10;
+	//int res = add(5, 9);
+	//cout << res << endl;
+	//print(res);
+	/*add(10);
+	print(x);
+	duplicate(x);
+	print(x)*/;
+
+	return 0;
 }
