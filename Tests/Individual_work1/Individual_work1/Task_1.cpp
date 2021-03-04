42f13cdebaaa37fc1423dff9231974acb3a8edf5
kretskyy <deonisius03@gmail.com>
2021-03-04 12:27:23 +0200

    Individual_work1_1

--- Tests/Individual_work1/Individual_work1/Task_1.cpp
+++ Tests/Individual_work1/Individual_work1/Task_1.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
-using namespace std;
-
-void main()
-{
-	int a;
-	cin >> a;
-
-	cout << pow(a, 2) << endl;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int rows;
+//	cout << "Enter number of rows=";
+//	cin >> rows;
+//
+//	for (int k = 1; k <= rows; ++k)
+//	{
+//		for (int l = 1; l <= rows; ++l)
+//		{
+//			cout << l << " ";
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//	system("pause");
+//}
--- Tests/Individual_work1/Individual_work1/Task_2.cpp
+++ Tests/Individual_work1/Individual_work1/Task_2.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
-void main1()
+int main()
 {
-	int a;
-	cin >> a;
-
-	cout << pow(a, 2) << endl;
+    int n, k = 0, l = 1, rem;
+    cout << "Input number in binary system:";
+    cin >> n;
+    while (n != 0)
+    {
+        rem = n % 10;
+        n = n / 10;
+        if (rem == 1)
+        {
+            k += l;
+        }
+        l *= 2;
+    }
+    cout << "Output number in decimial system:" << k;
+    return 0;
 }
-// Input: 2
-// Output: 4
+//Input number in binary system:1010
+//Output number in decimial system:10
+//Input number in binary system:10
+//Output number in decimial system:2
+//Input number in binary system:101
+//Output number in decimial system:5
+
