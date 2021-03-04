4fa8d1a40aecbbb197ce5e19d77843a268484926
kretskyy <deonisius03@gmail.com>
2021-03-04 12:30:06 +0200

    Individual_work1_2

--- Tests/Individual_work1/Individual_work1/Task_1.cpp
+++ Tests/Individual_work1/Individual_work1/Task_1.cpp
@@ -1,20 +1,20 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int rows;
-//	cout << "Enter number of rows=";
-//	cin >> rows;
-//
-//	for (int k = 1; k <= rows; ++k)
-//	{
-//		for (int l = 1; l <= rows; ++l)
-//		{
-//			cout << l << " ";
-//		}
-//		cout << endl;
-//	}
-//	return 0;
-//	system("pause");
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int rows;
+	cout << "Enter number of rows=";
+	cin >> rows;
+
+	for (int k = 1; k <= rows; k++)
+	{
+		for (int l = 1; l <= rows; l++)
+		{
+			cout << l << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+	system("pause");
+}
